84262500f667ff8443bc258a24a42f6947776806
李先营 <li.xianying@trs.com.cn>
2019-01-10 10:01:43 +0800

    跨域解决

--- src/com/taobao/controller/GetGoodsController.java
+++ src/com/taobao/controller/GetGoodsController.java
@@ -5,18 +5,18 @@ import com.taobao.entity.GoodsJson;
 import com.taobao.service.GoodsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.*;
+
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
+//部分跨域
+//@CrossOrigin(origins = "*", maxAge = 3600)
 @Controller
 
 public class GetGoodsController {
+
+
     @Autowired
     GoodsService goods_service;
 
